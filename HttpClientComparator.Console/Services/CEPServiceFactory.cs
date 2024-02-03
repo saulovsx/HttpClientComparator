056128e11268ca4550da9017b9ac9095af811deb
Saulo Vinícius Silva <saulovsx@gmail.com>
2024-02-03 12:04:27 -0300

    feat: Nova classe Factory para implementação dos serviços

---
+++ HttpClientComparator.Console/Services/CEPServiceFactory.cs
@@ -0,0 +1,27 @@
+﻿using HttpClientComparator.Console.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using HttpClientComparator.Console.Enums;
+
+namespace HttpClientComparator.Console.Services
+{
+    public class CEPServiceFactory : ICEPServiceFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CEPServiceFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public ICEPService CreateCEPService(TypeServiceEnum type)
+        {
+            return type switch
+            {
+                TypeServiceEnum.HttpClient => _serviceProvider.GetRequiredService<HttpClientCEPService>(),
+                TypeServiceEnum.RestSharp => _serviceProvider.GetRequiredService<RestSharpCEPService>(),
+                TypeServiceEnum.Flurl => _serviceProvider.GetRequiredService<FlurlCEPService>(),
+                _ => throw new ArgumentException("Invalid CEP service type"),
+            };
+        }        
+    }
+}
