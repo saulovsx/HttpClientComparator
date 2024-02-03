bf5291006f84640bc4d3de83afea7269fcb027c7
Saulo Vinícius Silva <saulovsx@gmail.com>
2024-02-03 12:05:32 -0300

    feat: Implementação de Benchmark para teste de comparação de performance entre serviços

---
+++ HttpClientComparator.Console/CEPServiceBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using HttpClientComparator.Console.Interfaces;
+using HttpClientComparator.Console.Services;
+using HttpClientComparator.Console.Enums;
+using Microsoft.Extensions.DependencyInjection;
+
+public class CEPServiceBenchmark
+{    
+    private readonly ICEPServiceFactory cepServiceFactory;
+
+    public CEPServiceBenchmark()
+    {        
+        var services = new ServiceCollection();
+        services.AddHttpClient();
+        services.AddTransient<IRestHttpClient, RestHttpClient>();
+        services.AddTransient<HttpClientCEPService>();
+        services.AddTransient<RestSharpCEPService>();
+        services.AddTransient<FlurlCEPService>();
+        services.AddTransient<ICEPServiceFactory, CEPServiceFactory>();
+
+        var serviceProvider = services.BuildServiceProvider();
+        cepServiceFactory = serviceProvider.GetRequiredService<ICEPServiceFactory>();        
+    }
+
+    [Benchmark]
+    public async Task TestHttpClientCEPService()
+    {
+        var httpClientServiceCEP = cepServiceFactory.CreateCEPService(TypeServiceEnum.HttpClient);        
+        await httpClientServiceCEP.GetCEPInfoAsync("01153000");
+    }
+
+    [Benchmark]
+    public async Task TestRestSharpCEPService()
+    {
+        var restSharpServiceCEP = cepServiceFactory.CreateCEPService(TypeServiceEnum.RestSharp);
+        await restSharpServiceCEP.GetCEPInfoAsync("01153000");
+    }
+
+    [Benchmark]
+    public async Task TestFlurlCEPService()
+    {
+        var flurlServiceCEP = cepServiceFactory.CreateCEPService(TypeServiceEnum.Flurl);
+        await flurlServiceCEP.GetCEPInfoAsync("01153000");
+    }
+}
