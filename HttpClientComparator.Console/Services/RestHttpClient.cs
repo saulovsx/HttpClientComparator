d10a7ca26aef5cc386fd01c7dfd4424614b3e614
Saulo Vinícius Silva <saulovsx@gmail.com>
2024-02-03 12:03:08 -0300

    feat: Nova classe base para implementação do HttpClient

---
+++ HttpClientComparator.Console/Services/RestHttpClient.cs
@@ -0,0 +1,35 @@
+﻿using HttpClientComparator.Console.Interfaces;
+
+namespace HttpClientComparator.Console.Services
+{
+    public class RestHttpClient : IRestHttpClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public RestHttpClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            //Configurações adicionais do httpClient
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            return await _httpClient.GetAsync(url);
+        }
+
+        public async Task<HttpResponseMessage> PostAsync(string url, HttpContent content)
+        {
+            return await _httpClient.PostAsync(url, content);
+        }
+
+        public async Task<HttpResponseMessage> PutAsync(string url, HttpContent content)
+        {
+            return await _httpClient.PutAsync(url, content);
+        }
+
+        public async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            return await _httpClient.DeleteAsync(url);
+        }
+    }
+}
